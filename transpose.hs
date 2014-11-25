9b20c479d568c3b802621839294a517655478ed8
Bart Massey <bart@cs.pdx.edu>
2014-11-25 02:18:38 -0800

    cleaned up transpose

--- transpose.hs
+++ transpose.hs
@@ -6,12 +6,19 @@
 import Data.List
 import System.Environment
 
+-- Group the text `cs` into groups of `k` characters,
+-- with the last group potentially short.
+gather :: Int -> String -> [String]
 gather k cs
   | null rest = [cs]
   | otherwise = first : gather k rest
   where
     (first, rest) = splitAt k cs
 
+-- Glue everything together, gather it into the right units,
+-- transpose it, glue it back together, and gather it into
+-- 78-char lines for printing.
+main :: IO ()
 main = do
   [countStr] <- getArgs
   interact (unlines . gather 78 . concat . transpose .
